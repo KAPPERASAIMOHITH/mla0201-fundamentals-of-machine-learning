bf823a474f944cfb70ce734dbd18e56413441260
KAPPERASAIMOHITH <113877277+KAPPERASAIMOHITH@users.noreply.github.com>
2023-08-30 16:26:49 +0530

    Update exp1-findsalgorithm.py

--- exp1-findsalgorithm.py
+++ exp1-findsalgorithm.py
@@ -1,21 +1,18 @@
-import csv
-a = []
-with open('enjoysport.csv', 'r') as csvfile:
-    for row in csv.reader(csvfile):
-        a.append(row)
-    print(a)
-print("\n The total number of training instances are : ",len(a))
-num_attribute = len(a[0])-1
-print("\n The initial hypothesis is : ")
-hypothesis = ['0']*num_attribute
-print(hypothesis)
-for i in range(0, len(a)):
- if a[i][num_attribute] == 'yes':
-  for j in range(0, num_attribute):
-      if hypothesis[j] == '0' or hypothesis[j] == a[i][j]:
-          hypothesis[j] = a[i][j]
-      else:
-          hypothesis[j] = '?'
- print("\n The hypothesis for the training instance {} is :\n" .format(i+1),hypothesis)
-print("\n The Maximally specific hypothesis for the training instance is ")
-print(hypothesis)
+import pandas as pd
+data = pd.read_csv("enjoysport.csv")
+def find_s_algorithm(data):
+    # Initialize the most specific hypothesis
+    hypothesis = ['0'] * (len(data.columns) - 1)
+    print("hypothesis",hypothesis);
+    
+    # Iterate through each positive example
+    for idx, row in data.iterrows():
+        if row['enjoysport'] == 'yes':
+            for i in range(len(hypothesis)):
+                if hypothesis[i] == '0':
+                    hypothesis[i] = row[i]
+                elif hypothesis[i] != row[i]:
+                    hypothesis[i] = '?'
+    return hypothesis
+specific_hypothesis = find_s_algorithm(data)
+print("Specific Hypothesis:", specific_hypothesis)
